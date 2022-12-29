03ad08cfe5920c6ca2ce8b4c1b395987288d69d5
yorhadev <rafael.takano.dev@gmail.com>
2022-12-29 14:06:25 -0300

    feat: adding dist

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./assets/style.css";
-import { appHome } from "./pages/home/home";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <main id="main"></main>
-`;
-
-appHome(document.querySelector<HTMLElement>("#main")!);
