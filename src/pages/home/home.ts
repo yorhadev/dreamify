b8cbbee47386d8e29005d74216a0c851ad0cb98a
yorhadev <rafael.takano.dev@gmail.com>
2022-12-29 12:23:46 -0300

    feat(home): added home title

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,10 +2,13 @@ import "./home.css";
 
 export function appHome(element: HTMLElement) {
   const homeInnerHTML = `
-        <div>
-            component home
-        </div>
-    `;
+    <div class="home">
+      <div class="home__title">
+        <h1>dreamify</h1>
+        <p>Create images from scratch based on a text prompt</p>
+      </div>
+    </div>
+  `;
 
   element.innerHTML = homeInnerHTML;
 }
