9d5a8102943089381347b77df817fc81527fcd90
yorhadev <rafael.takano.dev@gmail.com>
2022-12-29 14:04:28 -0300

    feat(components): created app form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import "./home.css";
+import { appForm } from "../../components/app-form/app-form";
 
 export function appHome(element: HTMLElement) {
   const homeInnerHTML = `
@@ -7,8 +8,16 @@ export function appHome(element: HTMLElement) {
         <h1>dreamify</h1>
         <p>Create images from scratch based on a text prompt</p>
       </div>
+
+      <div id="home__container" class="home__container">
+        <div>Your image will appear here</div>
+      </div>
+
+      <form id="home__form" class="home__form"></form>
     </div>
   `;
 
   element.innerHTML = homeInnerHTML;
+
+  appForm(document.querySelector<HTMLFormElement>("#home__form")!);
 }
